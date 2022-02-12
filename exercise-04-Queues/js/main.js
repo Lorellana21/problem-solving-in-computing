7176e6e4dc2866f7b546eb79f2137c2ceb698d47
Lorellana <lorellana.ordonez@gmail.com>
2022-02-12 10:52:36 +0100

    add readme

---
+++ exercise-04-Queues/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+import { MaxPriorityQueue } from "@datastructures-js/priority-queue";
+import { Stack } from "@datastructures-js/stack";
+
+
+const disk_tower = (disks_provided) => {
+    // Complete
+}
+
+disk_tower([4, 5, 1, 2, 3]).forEach((disks, day) => console.log('Day', day + 1, '=>', JSON.stringify(disks)));
