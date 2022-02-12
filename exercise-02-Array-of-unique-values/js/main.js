c576be701e31784ee4e7b2f27ae8a2a7b69118c0
Lorellana <lorellana.ordonez@gmail.com>
2022-02-12 10:45:14 +0100

    add readme

---
+++ exercise-02-Array-of-unique-values/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const intersection = (...arrays) => {
+
+
+    arr = np.array([12, 12, 9, 1, 2, 9, 8, 2, 1])
+
+
+
+    // Complete
+}
+
+const arr1 = [1, 2];
+const arr2 = [2, 3];
+console.log(intersection(arr1, arr2)) // [2]
+
+const arr3 = ['a', 'b'];
+const arr4 = ['b', 'c'];
+const arr5 = ['b', 'e', 'c'];
+console.log(intersection(arr3, arr4, arr5)) // ['b']
+
+const arr6 = ['b', 'b', 'e'];
+const arr7 = ['b', 'c', 'e'];
+const arr8 = ['b', 'e', 'c'];
+console.log(intersection(arr6, arr7, arr8)) // ['b', 'e']
---
+++ exercise-02-Array-of-unique-values/readme.md
@@ -0,0 +1,12 @@
+# Array of unique values
+
+Write a method that creates an array of unique values that are included in all given arrays
+
+Expected Result: ([1, 2], [2, 3]) => [2]
+Restriction: Use only arrays as data structures to solve the problem
+
+@param {?} arrays - Arrays of primitive data types
+
+@returns {Array}
+
+https://medium.com/@andrey.igorevich.borisov/10-javascript-exercises-with-arrays-c44eea129fba
