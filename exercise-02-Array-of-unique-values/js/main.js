b53cf81fbd9844121a8310e58f5219a39ece50ac
Lorellana <lorellana.ordonez@gmail.com>
2022-02-21 12:20:59 +0100

    done

--- exercise-02-Array-of-unique-values/js/main.js
+++ exercise-02-Array-of-unique-values/js/main.js
@@ -1,14 +1,57 @@
 'use strict';
 
+// const intersection = (...arrays) => {
+//     let result = [];
+//     //itero todos los arrays
+//     arrays.forEach(array => {
+//         //entro en cada uno de los elementos
+//         for (var i = 0; i < array.length; i++) {
+//             var element = array[i];
+//             //preguntar a element si esta en todos los arrays
+//             var inAll = true;//acumulador
+//             arrays.forEach(otherArray => {
+//                 inAll = inAll && otherArray.includes(element);
+//             });
+//             if (inAll && !result.includes(element)) {
+//                 result.push(element);
+//             }
+//         }
+//     });
+//     return result;
+// }
+
 const intersection = (...arrays) => {
+    let result = [];
+
+    //Iterar todos los arrays
+    arrays.forEach(array => {
+
+        //Iterar todos los elementos del array
+        for (var i = 0; i < array.length; i++) {
+            var shouldInclude = true;
+
+            //Saltar los elemntos si ya estan incluidos
+            if (!result.includes(array[i])) {
+
+                //comprobar si el elemento esta incluido en cada array
+                for (var j = 0; j < arrays.length; j++) {
+                    shouldInclude = shouldInclude && arrays[j].includes(array[i]);
+                }
+                //Añadir el elemento al array
+                if (shouldInclude) {
+                    result.push(array[i]);
+                }
+            }
 
 
-    arr = np.array([12, 12, 9, 1, 2, 9, 8, 2, 1])
+        }
+    });
+    return result;
 
-
-
-    // Complete
 }
+
+
+
 
 const arr1 = [1, 2];
 const arr2 = [2, 3];
