ef6f45f4fb7857c1d2398eabdbfa832c27bfed07
Lorellana <lorellana.ordonez@gmail.com>
2022-04-06 20:28:49 +0200

    feat(exrc03.js): improve solution

--- src/ECMAScript6-exercises/exrc01.js
+++ src/ECMAScript6-exercises/exrc01.js
@@ -12,14 +12,12 @@ for (const user in users) {
 
 
 //extraer datos: crear otro array solo con los nombres
-
 const names = users.map(user => user.name);
 console.log(names)
 
 //mejora: en vez de repetir la palabra "user", si lo que en realidad nos
 //importa es el "name", desarmamos el parámetro recibido(user) y obtenemos solo
 //la propiedad "name"
-
 const namesImproved = users.map(({ name }) => name);
 console.log(namesImproved)
 //los paréntesis son para poder desarmar el parámetro y obtener su propiedad
--- src/ECMAScript6-exercises/exrc03.js
+++ src/ECMAScript6-exercises/exrc03.js
@@ -3,11 +3,12 @@ const users = require('./users.json');
 //Obtener el número de usuarios que tienen website
 
 
-let result = 0;
-users.forEach((user) => {
-    if (user.website != undefined) {
-        result++;
-        console.log(result);
-    }
-})
+//1: filter users with website
+const usersWithWebsite = users.filter(user =>
+    user.website != null);
+
+//2: get the lenght of the new array of users with website
+console.log(usersWithWebsite.length)
+
+
 
