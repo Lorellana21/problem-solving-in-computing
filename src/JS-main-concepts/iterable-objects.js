9d27d6b34451b28bdcab55c5633d36d21d29cf5c
Lorellana <lorellana.ordonez@gmail.com>
2022-07-01 12:29:21 +0200

    feat(iterable-objects.js): add loops to iterate dimensional arrays

--- src/JS-main-concepts/iterable-objects.js
+++ src/JS-main-concepts/iterable-objects.js
@@ -1,4 +1,4 @@
-const iterable1 = new Map([[1, "a"], [2, "b"], [3, "c"]]);
+ const iterable1 = new Map([[1, "a"], [2, "b"], [3, "c"]]);
 
 for(let element of iterable1){
     console.log(`${element}`);
@@ -9,4 +9,28 @@ const iterable2 = new Set(myArray);
 
 for(let element of iterable2){
     console.log(`${element}`);
+}
+
+/*---------------------Recorrer un array de arrays -------------*/
+/*Necesito dos bucles for para poder recorrer la matriz. 
+Donde el primero recorrerá el array principal y el segundo 
+recorrerá cada uno de los array secundarios. */
+const matriz = [[1,2,3],[4,5,6],[7,8,9]];
+
+/*Los dos bucles concatenados quedan de la siguiente forma:*/
+for (let x = 0; x < matriz.length; x++) {
+    for (let y = 0; y < matriz[x].length; y++) {
+        console.log(matriz[x][y]);
+    }
+}
+
+//recorro el array global y obtengo los arrays interiores:
+for (let i = 0; i < matriz.length; i++){
+  let globalArray = matriz[i];
+  console.log(globalArray);
+  //recorro los arrays interiores y obtengo los elementos
+  for (let j = 0; j < globalArray.length; j++){
+    let element = globalArray[j];
+    console.log(element)
+  }
 }
